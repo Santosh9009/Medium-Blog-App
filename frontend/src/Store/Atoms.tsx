151809e0dc593010a604260725d3df2e775168f9
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-04-12 19:19:33 +0530

    added atoms

---
+++ frontend/src/Store/Atoms.tsx
@@ -0,0 +1,39 @@
+import { atom } from "recoil";
+
+interface Blog {
+  id: string;
+  title: string;
+  content: string;
+  publishDate: string;
+  author: {
+    name: string
+  }
+  // Add other properties as needed
+}
+interface user {
+  name:string ,
+  email:string ,
+  password:string ,
+}
+  
+export const allBlogs = atom<Blog[]>({
+  key:'allBlogState',
+  default:[]
+}) 
+
+
+export const myblogs = atom<Blog[]>({
+  key:'Myblogs',
+  default:[]
+})
+
+export const UserState = atom<user>({
+  key:'UserState',
+  default: {name:'', email:'',password:''} ,
+})
+
+
+
+
+
+
